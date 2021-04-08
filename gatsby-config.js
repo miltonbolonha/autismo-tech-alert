6ccb7e5e1a335269a2602a13212fd749e1e10cf6
bolonhadev <fala@bolonha.dev>
2021-04-07 21:16:10 -0300

    Styling + mailchimp icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
