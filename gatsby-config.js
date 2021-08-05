1f592b6d6ded06dc2be1b4981913ae948885ef9e
Milton ' Kiko ' Bolonha <miltonbolonha@gmail.com>
2021-08-05 20:27:34 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
-          "https://gmail.us4.list-manage.com/subscribe/post?u=f73925424e8a366f624728296&amp;id=222390269b", // string; add your MC list endpoint here; see instructions below
+          "", // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
